cc4f2e21891a8ae8783e663ecfac0868207cd818
Dexter Chan <dexterchann@gmail.com>
2020-03-12 02:36:44 +0000

    Added request for IP address

---
+++ iss.js
@@ -0,0 +1,21 @@
+const request = require("request");
+
+const fetchMyIP = (callback) => {
+  request("https://api.ipify.org?format=json", (error, response, body) => {
+    console.log("statusCode:", response.statusCode);
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
